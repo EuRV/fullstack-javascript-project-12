e5841dc740e9cfd10e318ed908d0abe1cfbe310e
EuRV <eugenirovensky@yandex.ru>
2023-06-01 18:55:19 +0600

    feat: create API server routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+const apiPath = '/api/v1';
+
+export default {
+  loginPath: () => [apiPath, 'login'].join('/'),
+  signupPath: () => [apiPath, 'signup'].join('/'),
+  dataPath: () => [apiPath, 'data'].join('/'),
+};
