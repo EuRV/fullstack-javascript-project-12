1b26428cb828e9aed312f76936b4ea6a49dd39f5
EuRV <eugenirovensky@yandex.ru>
2023-06-01 18:43:08 +0600

    feat: create a context provider

---
+++ frontend/src/components/AuthProvider.js
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable functional/no-expression-statements */
+import React, { useState, useMemo } from 'react';
+import AuthContext from '../context/index';
+
+const AuthProvider = ({ children }) => {
+  const currentUser = localStorage.getItem('userId');
+  const [loggedIn, setLoggedIn] = useState(Boolean(currentUser));
+  const [user, setUser] = useState(currentUser || null);
+
+  const signIn = (data) => {
+    setUser(data);
+    setLoggedIn(true);
+  };
+  const signOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  const cachedValue = useMemo(() => (
+    {
+      loggedIn, user, signIn, signOut,
+    }
+  ), [loggedIn, user, signIn]);
+
+  return (
+    <AuthContext.Provider value={cachedValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
