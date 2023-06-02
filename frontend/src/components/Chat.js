559443e768211eda011a0785d547227d774629f0
EuRV <eugenirovensky@yandex.ru>
2023-06-02 21:26:15 +0600

    feat: implement a Chat page

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Chat = () => (
+  <Container className="h-100 my-4 overflow-hidden rounded shadow" />
+);
+
+export default Chat;
