cd53df99eae40e2a3289782e8637dafba72a188e
EuRV <eugenirovensky@yandex.ru>
2023-06-02 21:35:55 +0600

    feat: add authorized user data upload

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,8 +1,29 @@
-import React from 'react';
+/* eslint-disable functional/no-expression-statements */
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 
-const Chat = () => (
-  <Container className="h-100 my-4 overflow-hidden rounded shadow" />
-);
+import useAuth from '../hooks';
+import routes from '../routes';
+
+const Chat = () => {
+  const { user: { token } } = useAuth();
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    axios.get(routes.dataPath(), {
+      headers:
+        { Authorization: `Bearer ${token}` },
+    })
+      .then(({ data }) => setState(data));
+  }, [token]);
+
+  // eslint-disable-next-line functional/no-expression-statements
+  console.log(state);
+
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded shadow" />
+  );
+};
 
 export default Chat;
