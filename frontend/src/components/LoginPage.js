b59b9179e50818ac28169daffc4200d28506b999
EuRV <eugenirovensky@yandex.ru>
2023-06-02 17:22:11 +0600

    refactor: add variables from the dictionary

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -6,6 +6,7 @@ import {
   Button, Card, Form, FloatingLabel, Row, Col, Container,
 } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import useAuth from '../hooks/index';
 import routes from '../routes';
 
@@ -14,6 +15,7 @@ const LoginPage = () => {
   const [authFailed, setAuthFailed] = useState(false);
   const inputRef = useRef();
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   useEffect(() => {
     inputRef.current.focus();
@@ -51,17 +53,17 @@ const LoginPage = () => {
             <Card.Body className="row p-5">
               <Col md={6} className="col-12 d-flex align-items-center justify-content-center" />
               <Form className="col-12 col-md-6 mt-3 mt-mb-0" onSubmit={formik.handleSubmit}>
-                <h1 className="text-center mb-4">Войти</h1>
+                <h1 className="text-center mb-4">{t('logIn.title')}</h1>
                 <fieldset disabled={formik.isSubmitting}>
                   <FloatingLabel
                     className="mb-3"
                     controlId="username"
-                    label="Ваш ник"
+                    label={t('logIn.username')}
                   >
                     <Form.Control
                       name="username"
                       required
-                      placeholder="Ваш ник"
+                      placeholder={t('logIn.username')}
                       onBlur={formik.handleBlur}
                       onChange={formik.handleChange}
                       value={formik.values.username}
@@ -72,31 +74,30 @@ const LoginPage = () => {
                   <FloatingLabel
                     className="mb-4"
                     controlId="password"
-                    label="Пароль"
+                    label={t('logIn.password')}
                   >
                     <Form.Control
                       type="password"
                       name="password"
                       required
-                      placeholder="Пароль"
+                      placeholder={t('logIn.password')}
                       onBlur={formik.handleBlur}
                       onChange={formik.handleChange}
                       value={formik.values.password}
                       isInvalid={authFailed}
                     />
-                    <Form.Control.Feedback type="invalid" tooltip>Неверные имя пользователя или пароль</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid" tooltip>{t('errors.invalidLoginPassword')}</Form.Control.Feedback>
                   </FloatingLabel>
                   <Button variant="outline-primary" type="submit" className="w-100 mb-3">
-                    Войти
+                    {t('logIn.loginButton')}
                   </Button>
                 </fieldset>
               </Form>
             </Card.Body>
             <Card.Footer className="p-4">
               <div className="text-center">
-                <span>Нет аккаунта?</span>
-                {' '}
-                <a href="/signup">Регистрация</a>
+                <span>{t('logIn.footerHeading')}</span>
+                <a href="/signup">{t('logIn.signUpLink')}</a>
               </div>
             </Card.Footer>
           </Card>
