c2565aa629c3515024ebad970040bd35a760e742
EuRV <eugenirovensky@yandex.ru>
2023-06-02 21:27:39 +0600

    feat: add routing to the Chat page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
+import { ToastContainer } from 'react-bootstrap';
 
 import Panel from './components/Panel';
 import LoginPage from './components/LoginPage';
 import NotFoundPage from './components/NotFoundPage';
 import PrivateRoute from './components/PrivatePage';
+import Chat from './components/Chat';
 
 const App = () => (
   <div className="d-flex flex-column h-100">
     <Router>
       <Panel />
       <Routes>
-        <Route path="/" element={<PrivateRoute />} />
+        <Route
+          path="/"
+          element={(
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          )}
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <ToastContainer />
     </Router>
   </div>
 );
