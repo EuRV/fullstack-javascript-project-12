3560830d9062b3d0bbc4ca929dcfb78ad30af9a0
EuRV <eugenirovensky@yandex.ru>
2023-06-02 17:06:17 +0600

    feat: translation ru

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    logIn: {
+      title: 'Войти',
+      username: 'Ваш ник',
+      password: 'Пароль',
+      loginButton: 'Войти',
+      footerHeading: 'Нет аккаунта? ',
+      signUpLink: 'Регистрация',
+    },
+    errors: {
+      invalidLoginPassword: 'Неверные имя пользователя или пароль',
+    },
+  },
+};
