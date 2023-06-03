f936144cd57bda2722d2d0a9090974143319927c
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:56:39 +0600

    feat: implement channel rendering and navigation

--- frontend/src/components/chatComponents/ChatChannels.js
+++ frontend/src/components/chatComponents/ChatChannels.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { Col, Nav } from 'react-bootstrap';
 
-const ChatChannels = () => (
-  <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex" />
-);
+import { getChannels } from '../../redux/selectors';
+import Channel from './Channel';
+
+const ChatChannels = () => {
+  const { t } = useTranslation();
+  const channels = useSelector(getChannels);
+  return (
+    <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+        <b>{t('chat.channels')}</b>
+        <button type="button" className="p-0 text-primary btn btn-group-vertical">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+          <span className="visually-hidden">+</span>
+        </button>
+      </div>
+      <Nav id="channels-box" as="ul" variant="pills" className="flex-column px-2 mb-3 overflow-auto h-100 d-block" fill>
+        {channels.map((channel) => (
+          <Channel key={channel.id} channel={channel} />
+        ))}
+      </Nav>
+    </Col>
+  );
+};
 
 export default ChatChannels;
