3c0a8e34f17b133fb5c79fbad75734acc26f5b78
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:54:40 +0600

    feat: implement messages rendering

--- frontend/src/components/chatComponents/ChatMessages.js
+++ frontend/src/components/chatComponents/ChatMessages.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Col } from 'react-bootstrap';
 
-const ChatMessages = () => (
-  <Col className="p-0 h-100" />
-);
+import { getMessagesCurrentChannel } from '../../redux/selectors';
+import Message from './Message';
+import MessageHeader from './MessagesHeader';
+
+const ChatMessages = () => {
+  const messages = useSelector(getMessagesCurrentChannel);
+  const { length } = messages;
+
+  return (
+    <Col className="p-0 h-100">
+      <MessageHeader />
+      <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+        {length > 0 && messages
+          .map((message) => <Message key={message.id} message={message} />)}
+      </div>
+    </Col>
+  );
+};
 
 export default ChatMessages;
