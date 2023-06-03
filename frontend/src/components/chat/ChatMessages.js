241cfb4ab55fb04327a3e6bab178f60293fc13c1
EuRV <eugenirovensky@yandex.ru>
2023-06-04 02:11:03 +0600

    feat: add a message entry form

--- frontend/src/components/chat/ChatMessages.js
+++ frontend/src/components/chat/ChatMessages.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Col } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 
 import { getMessagesCurrentChannel } from '../../redux/selectors';
 import Message from './Message';
 import MessageHeader from './MessageHeader';
+import MessageFooter from './MessageFooter';
 
 const ChatMessages = () => {
   const messages = useSelector(getMessagesCurrentChannel);
@@ -12,11 +13,14 @@ const ChatMessages = () => {
 
   return (
     <Col className="p-0 h-100">
-      <MessageHeader />
-      <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-        {length > 0 && messages
-          .map((message) => <Message key={message.id} message={message} />)}
-      </div>
+      <Container className="d-flex flex-column h-100">
+        <MessageHeader />
+        <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+          {length > 0 && messages
+            .map((message) => <Message key={message.id} message={message} />)}
+        </div>
+        <MessageFooter />
+      </Container>
     </Col>
   );
 };
