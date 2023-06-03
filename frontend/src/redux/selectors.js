e8363ef310f76ef82c959552de61480399a203ee
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:41:52 +0600

    feat: implement selectors

---
+++ frontend/src/redux/selectors.js
@@ -0,0 +1,27 @@
+import { selectors as channelsSelectors } from './slices/channelsSlice';
+import { selectors as messagesSelectors } from './slices/messagesSlices';
+
+const getChannels = (state) => channelsSelectors.selectAll(state);
+
+const getCurrentChannelId = (state) => {
+  const { currentChannelId } = state.channels;
+  return currentChannelId;
+};
+
+const getCurrentChannel = (state) => {
+  const { currentChannelId } = state.channels;
+  return channelsSelectors.selectById(state, currentChannelId);
+};
+
+const getMessagesCurrentChannel = (state) => {
+  const { currentChannelId } = state.channels;
+  const messagesAll = messagesSelectors.selectAll(state);
+  return messagesAll.filter(({ channelId }) => channelId === currentChannelId);
+};
+
+export {
+  getChannels,
+  getCurrentChannelId,
+  getCurrentChannel,
+  getMessagesCurrentChannel,
+};
