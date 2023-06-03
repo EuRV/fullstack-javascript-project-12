8f2e17bc633c1b6fc7abc1878ea3731c191ea2d0
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:48:32 +0600

    feat: add a loading status spinner

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,34 +1,53 @@
+/* eslint-disable functional/no-conditional-statements */
 /* eslint-disable functional/no-expression-statements */
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Container, Row } from 'react-bootstrap';
-
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Row, Spinner } from 'react-bootstrap';
+import { fetchAuthData } from '../redux/slices/loaderSlices';
 import useAuth from '../hooks';
-import { actions as channelsActions } from '../redux/slices/channelsSlice';
-import { actions as messagesActions } from '../redux/slices/messagesSlices';
-import { getData } from '../api/controllers';
+
 import ChatChannels from './chatComponents/ChatChannels';
 import ChatMessages from './chatComponents/ChatMessages';
 
 const Chat = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
   const { user: { token } } = useAuth();
+  const { loadingStatus, error } = useSelector((state) => state.loader);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const { channels, messages } = await getData(token);
-      dispatch(channelsActions.addChannels(channels));
-      dispatch(messagesActions.addMessages(messages));
-    };
-    fetchData();
-  });
+    dispatch(fetchAuthData(token));
+  }, [dispatch, token]);
 
-  return (
+  useEffect(() => {
+    if (loadingStatus === 'success') {
+      setLoading(true);
+    }
+    if (loadingStatus === 'failed') {
+      if (error.status === 401) {
+        return;
+      }
+      if (error === 'AxiosError') {
+        toast.error('error.network');
+        return;
+      }
+      toast.error('error.unknow');
+    }
+  }, [loadingStatus, error]);
+
+  return loading ? (
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
       <Row className="h-100 bg-white flex-md-row">
         <ChatChannels />
         <ChatMessages />
       </Row>
+    </Container>
+  ) : (
+    <Container>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     </Container>
   );
 };
