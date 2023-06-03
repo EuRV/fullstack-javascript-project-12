f4b5a8e3e16a7851a750a691a7f218ce002721f1
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:27:09 +0600

    feat: add reducers
    Add reducers of the current channel and data loading during authentication

--- frontend/src/redux/slices/channelsSlice.js
+++ frontend/src/redux/slices/channelsSlice.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable functional/no-expression-statements */
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { fetchAuthData } from './loaderSlices';
 
 const channelsAdapter = createEntityAdapter();
-const initialState = channelsAdapter.getInitialState();
+const initialState = channelsAdapter.getInitialState({
+  currentChannelId: null,
+});
 
 const channelsSlice = createSlice({
   name: 'channels',
@@ -9,8 +14,18 @@ const channelsSlice = createSlice({
   reducers: {
     addChannels: channelsAdapter.addMany,
     addChannel: channelsAdapter.addOne,
+    setCurrentChannel: ((state, { payload }) => {
+      state.currentChannelId = payload;
+    }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAuthData.fulfilled, (state, { payload }) => {
+      channelsAdapter.addMany(state, payload.channels);
+      state.currentChannelId = payload.currentChannelId;
+    });
   },
 });
 
 export const { actions } = channelsSlice;
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
 export default channelsSlice.reducer;
