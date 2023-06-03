5da5a1e14888770599495295cebcac815748d3d9
EuRV <eugenirovensky@yandex.ru>
2023-06-04 00:40:36 +0600

    feat: add a load reducer to the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './slices/channelsSlice';
 import messagesReducer from './slices/messagesSlices';
+import loaderReducer from './slices/loaderSlices';
 
 export default configureStore({
   reducer: {
+    loader: loaderReducer,
     channels: channelsReducer,
     messages: messagesReducer,
   },
