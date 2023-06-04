070aa488c8f4183c96864e2e0a59dc6c590e36b5
EuRV <eugenirovensky@yandex.ru>
2023-06-04 19:19:50 +0600

    feat: implement data loading via websocket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,59 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router, Routes, Route,
-} from 'react-router-dom';
+/* eslint-disable functional/no-conditional-statements */
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable functional/no-expression-statements */
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-bootstrap';
+import { io } from 'socket.io-client';
+import router from './router';
 
-import Panel from './components/Panel';
-import LoginPage from './components/LoginPage';
-import NotFoundPage from './components/NotFoundPage';
-import PrivateRoute from './components/PrivatePage';
-import Chat from './components/chat/Chat';
+import store from './redux/store';
+import { actions as messageActions } from './redux/slices/messagesSlices';
+import { actions as channelActions } from './redux/slices/channelsSlice';
+import AuthProvider from './context/AuthProvider';
+import { ChatApiContext } from './context';
 
-const App = () => (
-  <div className="d-flex flex-column h-100">
-    <Router>
-      <Panel />
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <PrivateRoute>
-              <Chat />
-            </PrivateRoute>
-          )}
-        />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <ToastContainer />
-    </Router>
-  </div>
-);
+const App = () => {
+  const { dispatch } = store;
+  const socket = io();
+
+  socket.on('newMessage', (payload) => {
+    dispatch(messageActions.addMessage(payload));
+  });
+  socket.on('newChannel', (payload) => {
+    dispatch(channelActions.addChannel(payload));
+  });
+
+  const promiseWrapper = (...args) => new Promise((resolve, reject) => {
+    socket.emit(...args, (response) => {
+      if (response.status === 'ok') {
+        resolve(response.data);
+      }
+      reject(response.error);
+    });
+  });
+
+  const api = {
+    sendMessage: (message) => promiseWrapper('newMessage', message),
+    addChannel: (channel) => promiseWrapper('newChannel', channel),
+    removeChannel: (channel) => promiseWrapper('removeChannel', channel),
+    renameChannel: (channel) => promiseWrapper('renameChannel', channel),
+  };
+
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <ChatApiContext.Provider value={api}>
+          <div className="d-flex flex-column h-100">
+            <RouterProvider router={router}>
+              <ToastContainer />
+            </RouterProvider>
+          </div>
+        </ChatApiContext.Provider>
+      </AuthProvider>
+    </Provider>
+  );
+};
 
 export default App;
