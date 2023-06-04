0f05928b6f955622fe57572dc6a924f7e077243a
EuRV <eugenirovensky@yandex.ru>
2023-06-04 19:23:40 +0600

    refactor: implement params transmission via props

--- frontend/src/components/chat/ChatMessages.js
+++ frontend/src/components/chat/ChatMessages.js
@@ -2,24 +2,25 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Col, Container } from 'react-bootstrap';
 
-import { getMessagesCurrentChannel } from '../../redux/selectors';
+import { getCurrentChannel, getMessagesCurrentChannel } from '../../redux/selectors';
 import Message from './Message';
 import MessageHeader from './MessageHeader';
 import MessageFooter from './MessageFooter';
 
 const ChatMessages = () => {
+  const { id, name } = useSelector(getCurrentChannel);
   const messages = useSelector(getMessagesCurrentChannel);
   const { length } = messages;
 
   return (
     <Col className="p-0 h-100">
       <Container className="d-flex flex-column h-100">
-        <MessageHeader />
+        <MessageHeader name={name} length={length} />
         <div id="messages-box" className="chat-messages overflow-auto px-5 ">
           {length > 0 && messages
             .map((message) => <Message key={message.id} message={message} />)}
         </div>
-        <MessageFooter />
+        <MessageFooter channelId={id} />
       </Container>
     </Col>
   );
