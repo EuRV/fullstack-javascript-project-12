811dd493ec664063a0ad2340b90a7268ff91b46b
EuRV <eugenirovensky@yandex.ru>
2023-06-04 18:49:18 +0600

    refactor: relocate the render to App component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,26 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './locales/index';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './redux/store';
-import AuthProvider from './context/AuthProvider';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // eslint-disable-next-line functional/no-expression-statements
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
