274ed21a048cc9f4c52b62ca61776b35a383bd38
EuRV <eugenirovensky@yandex.ru>
2023-06-04 19:13:44 +0600

    refactor: get parameters from props

--- frontend/src/components/chat/MessageHeader.js
+++ frontend/src/components/chat/MessageHeader.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { getCurrentChannel, getMessagesCurrentChannel } from '../../redux/selectors';
 
-const MessageHeader = () => {
+const MessageHeader = ({ name, length }) => {
   const { t } = useTranslation();
-  const { name } = useSelector(getCurrentChannel);
-  const { length } = useSelector(getMessagesCurrentChannel);
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
       <p className="m-0"><b>{`# ${name}`}</b></p>
