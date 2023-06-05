38a2b637b7e5a1bebb35106be036e4baa24f50f3
EuRV <eugenirovensky@yandex.ru>
2023-06-05 17:14:25 +0600

    refactor: change styles of elements

--- frontend/src/components/chat/ChatMessages.js
+++ frontend/src/components/chat/ChatMessages.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Col, Container } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 import { getCurrentChannel, getMessagesCurrentChannel } from '../../redux/selectors';
 import Message from './Message';
@@ -14,14 +14,14 @@ const ChatMessages = () => {
 
   return (
     <Col className="p-0 h-100">
-      <Container className="d-flex flex-column h-100">
+      <div className="d-flex flex-column h-100">
         <MessageHeader name={name} length={length} />
         <div id="messages-box" className="chat-messages overflow-auto px-5 ">
           {length > 0 && messages
             .map((message) => <Message key={message.id} message={message} />)}
         </div>
         <MessageFoorm channelId={id} />
-      </Container>
+      </div>
     </Col>
   );
 };
