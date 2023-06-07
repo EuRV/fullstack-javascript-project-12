0bfef0dcdd2ada85fc9882e2bc8e9b8e6178589a
EuRV <eugenirovensky@yandex.ru>
2023-06-07 11:05:16 +0600

    feat: implement the form change

--- frontend/src/components/Modals.js
+++ frontend/src/components/Modals.js
@@ -1,16 +1,33 @@
+/* eslint-disable functional/no-expression-statements */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Button, Modal, Form,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
+import { closeModal } from '../redux/slices/modalsSlice';
 
 const Modals = () => {
+  const dispatch = useDispatch();
   const { t } = useTranslation();
+  const { isOpened } = useSelector((state) => state.modal);
+
+  const handleColse = () => {
+    dispatch(closeModal());
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+  });
+
+  console.log(formik.values);
   return (
-    <Modal centered>
-      <Modal.Header>
+    <Modal show={isOpened} onHide={handleColse} centered>
+      <Modal.Header closeButton>
         <Modal.Title>{t('modals.addChannel')}</Modal.Title>
-        <Button aria-label="Close" data-bs-dismiss="modal" variant="close" />
       </Modal.Header>
       <Modal.Body>
         <Form>
@@ -18,12 +35,15 @@ const Modals = () => {
             <Form.Control
               name="name"
               className="mb-2"
-              value=""
+              required
+              value={formik.values.name}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
             />
             <Form.Label visuallyHidden>{t('modals.channelName')}</Form.Label>
             <Form.Control.Feedback type="invalid" />
             <div className="d-flex justify-content-end">
-              <Button variant="secondary" className="me-2">
+              <Button variant="secondary" className="me-2" onClick={handleColse}>
                 {t('modals.cancel')}
               </Button>
               <Button type="submit" variant="primary">
