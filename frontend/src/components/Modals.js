2861282bea92b72e5ae8f857e21272ba4520875d
EuRV <eugenirovensky@yandex.ru>
2023-06-07 13:48:51 +0600

    feat: implement channel name validation

--- frontend/src/components/Modals.js
+++ frontend/src/components/Modals.js
@@ -1,36 +1,53 @@
 /* eslint-disable functional/no-expression-statements */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Button, Modal, Form,
-} from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { closeModal } from '../redux/slices/modalsSlice';
+import { getChannels } from '../redux/selectors';
+
+yup.setLocale({
+  mixed: {
+    notOneOf: 'errors.duplicationChannel',
+  },
+});
 
 const Modals = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
   const { isOpened } = useSelector((state) => state.modal);
+  const channelNames = useSelector(getChannels).map(({ name }) => name);
 
   const handleColse = () => {
     dispatch(closeModal());
   };
 
+  const validationSchema = yup.object({
+    name: yup.string().notOneOf(channelNames),
+  });
+
   const formik = useFormik({
     initialValues: {
       name: '',
     },
+    validationSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values) => {
+      console.log(!formik.errors.name, values.name);
+    },
   });
 
-  console.log(formik.values);
+  // console.log(formik.errors.name);
   return (
     <Modal show={isOpened} onHide={handleColse} centered>
       <Modal.Header closeButton>
         <Modal.Title>{t('modals.addChannel')}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form noValidate onSubmit={formik.handleSubmit}>
           <Form.Group controlId="name">
             <Form.Control
               name="name"
@@ -39,9 +56,10 @@ const Modals = () => {
               value={formik.values.name}
               onBlur={formik.handleBlur}
               onChange={formik.handleChange}
+              isInvalid={!!formik.errors.name}
             />
             <Form.Label visuallyHidden>{t('modals.channelName')}</Form.Label>
-            <Form.Control.Feedback type="invalid" />
+            <Form.Control.Feedback type="invalid">{t(formik.errors.name)}</Form.Control.Feedback>
             <div className="d-flex justify-content-end">
               <Button variant="secondary" className="me-2" onClick={handleColse}>
                 {t('modals.cancel')}
