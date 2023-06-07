2a2f58cf9115277c6f276a55359e002e1cd933d3
EuRV <eugenirovensky@yandex.ru>
2023-06-07 19:26:30 +0600

    fix: add data upload

--- frontend/src/api/controllers.js
+++ frontend/src/api/controllers.js
@@ -6,5 +6,13 @@ const logIn = async ({ username, password }) => {
   return data;
 };
 
+const fetchData = async (token) => {
+  const { data } = await axios.get(routes.dataPath(), {
+    headers:
+    { Authorization: `Bearer ${token}` },
+  });
+  return data;
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { logIn };
+export { logIn, fetchData };
