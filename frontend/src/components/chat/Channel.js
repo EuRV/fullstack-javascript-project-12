a45d6bdc76f6d336fd17d1257a293ebd4240ff9e
EuRV <eugenirovensky@yandex.ru>
2023-06-09 16:34:57 +0600

    feat: add a removable component
    Implement element selection, dropdown list

--- frontend/src/components/chat/Channel.js
+++ frontend/src/components/chat/Channel.js
@@ -1,7 +1,9 @@
 /* eslint-disable functional/no-expression-statements */
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, Nav } from 'react-bootstrap';
+import {
+  Button, Nav, ButtonGroup, Dropdown,
+} from 'react-bootstrap';
 import { actions } from '../../redux/slices/channelsSlice';
 
 const Channel = ({ channel, currentChannelId }) => {
@@ -11,7 +13,34 @@ const Channel = ({ channel, currentChannelId }) => {
     dispatch(actions.setCurrentChannel(id));
   };
 
-  return (
+  console.log(channel);
+
+  return channel.removable ? (
+    <Nav.Item as="li" className="w-100">
+      <Dropdown
+        className="d-flex"
+        as={ButtonGroup}
+      >
+        <Button
+          className="w-100 rounded-0 text-start text-truncate"
+          onClick={() => handleChoose(channel.id)}
+          variant={channel.id === currentChannelId ? 'secondary' : null}
+        >
+          <span className="me-1">#</span>
+          {channel.name}
+        </Button>
+        <Dropdown.Toggle
+          className="flex-grow-0"
+          variant={channel.id === currentChannelId ? 'secondary' : null}
+          split
+        />
+        <Dropdown.Menu>
+          <Dropdown.Item>Удалить</Dropdown.Item>
+          <Dropdown.Item>Переименовать</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Nav.Item>
+  ) : (
     <Nav.Item as="li" className="w-100">
       <Button
         className="w-100 rounded-0 text-start"
