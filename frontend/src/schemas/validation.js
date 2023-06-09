927d8c3c26717f1925e39a3b3b2829770daf4dc8
EuRV <eugenirovensky@yandex.ru>
2023-06-09 18:59:18 +0600

    refactor: add a channel validation scheme

--- frontend/src/schemas/validation.js
+++ frontend/src/schemas/validation.js
@@ -1,7 +1,29 @@
+/* eslint-disable functional/no-expression-statements */
 import * as yup from 'yup';
+
+yup.setLocale({
+  mixed: {
+    notOneOf: 'errors.duplicationChannel',
+    required: 'errors.requiredField',
+  },
+  string: {
+    min: 'errors.lengthChannelName',
+    max: 'errors.lengthChannelName',
+  },
+});
+
+const channelValidate = (channels) => yup.object({
+  name: yup
+    .string()
+    .trim()
+    .required()
+    .min(3)
+    .max(20)
+    .notOneOf(channels),
+});
 
 const messageValidate = yup.object({
   body: yup.string().required().min(5).max(50),
 });
 
-export default messageValidate;
+export { channelValidate, messageValidate };
