9635693513f9cbd1440d7c6cfa185afeb24cfd84
EuRV <eugenirovensky@yandex.ru>
2023-06-10 01:45:13 +0600

    rafactor: rename the import and variables

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -6,12 +6,12 @@ import { Container, Row, Spinner } from 'react-bootstrap';
 // import { fetchAuthData } from '../../redux/slices/loaderSlices';
 import { useAuth } from '../hooks';
 import { fetchData } from '../api/controllers';
-import { actions as channelActions } from '../redux/slices/channelsSlice';
+import { addChannels, setCurrentChannel } from '../redux/slices/channelsSlice';
 import { actions as messageActions } from '../redux/slices/messagesSlices';
 
 import Channels from './chat/Channels';
 import Messages from './chat/Messages';
-// import Modals from '../Modals';
+import Modals from './Modals';
 
 const Chat = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,8 +22,8 @@ const Chat = () => {
     const getData = async () => {
       try {
         const data = await fetchData(user.token);
-        dispatch(channelActions.addChannels(data.channels));
-        dispatch(channelActions.setCurrentChannel(data.currentChannelId));
+        dispatch(addChannels(data.channels));
+        dispatch(setCurrentChannel(data.currentChannelId));
         dispatch(messageActions.addMessages(data.messages));
         // eslint-disable-next-line no-unused-expressions
         data ? setIsLoading(true) : setIsLoading(false);
@@ -37,12 +37,15 @@ const Chat = () => {
   }, []);
 
   return isLoading ? (
-    <Container className="h-100 my-4 overflow-hidden rounded shadow">
-      <Row className="h-100 bg-white flex-md-row">
-        <Channels />
-        <Messages />
-      </Row>
-    </Container>
+    <>
+      <Container className="h-100 my-4 overflow-hidden rounded shadow">
+        <Row className="h-100 bg-white flex-md-row">
+          <Channels />
+          <Messages />
+        </Row>
+      </Container>
+      <Modals />
+    </>
   ) : (
     <Container className="h-100 max-height-90 overflow-hidden rounded shadow d-flex justify-content-center align-items-center">
       <Spinner animation="border" role="status">
