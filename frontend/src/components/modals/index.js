77d6ceb7b9d9b4b9839c70a9eb6c94233a8e155b
EuRV <eugenirovensky@yandex.ru>
2023-06-10 15:37:07 +0600

    feat: add modal window selection

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,9 +1,11 @@
 import ModalAddig from './ModalAdding';
 import ModalRemove from './ModalRemove';
+import ModalRename from './ModalReaname';
 
 const modals = {
   addChannel: ModalAddig,
   removeChannel: ModalRemove,
+  renameChannel: ModalRename,
 };
 
 export default (modalName) => modals[modalName];
