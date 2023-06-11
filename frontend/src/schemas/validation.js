83bdfc590c58082dd8815ad62926fb3e8ea68681
EuRV <eugenirovensky@yandex.ru>
2023-06-12 00:13:38 +0600

    feat: add a SignUp component validation scheme

--- frontend/src/schemas/validation.js
+++ frontend/src/schemas/validation.js
@@ -4,11 +4,12 @@ import * as yup from 'yup';
 yup.setLocale({
   mixed: {
     notOneOf: 'errors.duplicationChannel',
+    test: 'errors.confirmed',
     required: 'errors.requiredField',
   },
   string: {
-    min: 'errors.lengthChannelName',
-    max: 'errors.lengthChannelName',
+    min: 'errors.lengthName',
+    max: 'errors.lengthName',
   },
 });
 
@@ -23,7 +24,24 @@ const channelValidate = (channels) => yup.object({
 });
 
 const messageValidate = yup.object({
-  body: yup.string().required().min(1).max(50),
+  body: yup
+    .string()
+    .required(),
 });
 
-export { channelValidate, messageValidate };
+const signUpValidate = yup.object({
+  username: yup
+    .string()
+    .required()
+    .min(3)
+    .max(20),
+  password: yup
+    .string()
+    .required()
+    .min(6, 'errors.lengthPassword'),
+  confirmPassword: yup
+    .string()
+    .test('confirmPassvord', (value, context) => value === context.parent.password),
+});
+
+export { channelValidate, messageValidate, signUpValidate };
