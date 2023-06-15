ca2d223906c18308fc2a3e5ab4d1ab04b12cd834
EuRV <eugenirovensky@yandex.ru>
2023-06-16 03:24:56 +0600

    refactor: move localStorage.setitem to signIn

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -32,7 +32,6 @@ const SignUp = () => {
       const { username, password } = values;
       try {
         const registeredUser = await signUp({ username, password });
-        localStorage.setItem('userId', JSON.stringify(registeredUser));
         auth.signIn(registeredUser);
         navigate('/');
       } catch (error) {
