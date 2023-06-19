a2fde3a0e222b2a0e4f162f7778463a80e6b2d48
EuRV <eugenirovensky@yandex.ru>
2023-06-19 17:48:08 +0600

    feat: implement channels scrolling

--- frontend/src/components/chat/Channels.js
+++ frontend/src/components/chat/Channels.js
@@ -1,10 +1,12 @@
+/* eslint-disable functional/no-conditional-statements */
+/* eslint-disable no-undef */
 /* eslint-disable functional/no-expression-statements */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Col, Nav } from 'react-bootstrap';
 
-import { getChannels, getCurrentChannelId } from '../../redux/selectors';
+import { getChannels, getCurrentChannelId, getDefaultChannelId } from '../../redux/selectors';
 import { open } from '../../redux/slices/modalsSlice';
 import ChannelsHeader from './ChannelsHeader';
 import Channel from './Channel';
@@ -13,7 +15,18 @@ const Channels = () => {
   const { t } = useTranslation();
   const channels = useSelector(getChannels);
   const currentChannelId = useSelector(getCurrentChannelId);
+  const defaultChannelId = useSelector(getDefaultChannelId);
+  const lastChannelsItemId = channels.at(-1)?.id;
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentChannelId === defaultChannelId) {
+      animateScroll.scrollToTop({ containerId: 'channels-box', delay: 0, duration: 0 });
+    }
+    if (currentChannelId === lastChannelsItemId) {
+      animateScroll.scrollToBottom({ containerId: 'channels-box', delay: 0, duration: 0 });
+    }
+  }, [currentChannelId, lastChannelsItemId, defaultChannelId]);
 
   const handleClick = () => {
     dispatch(open({ type: 'addChannel' }));
